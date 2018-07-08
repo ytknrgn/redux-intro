6807df9fda64fa2f5244590c8cabba47f9c07485
dmitrigrabov <d.grabov@gmail.com>
2018-07-08 10:51:48 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import VotingButtons from './VotingButtons';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <VotingButtons />
+      </div>
+    )
+  }
+}
+
+export default App;
